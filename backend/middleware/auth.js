c323ce37b63255d864a28d5e1ef8b515501720e7
ShivamBajpai04 <shivambajpai04@gmail.com>
2024-10-12 16:26:58 +0530

    i think i fixed it now

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const auth = async (req, res, next) => {
+const auth = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1]; // Bearer token
   if (!token)
     return res.status(401).json({ msg: "No token, authorization denied" });
 
   try {
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded
     next();
   } catch (err) {
