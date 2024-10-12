7da1074e4b4495e9cd2b27c05c26ce08d7a84b2c
ShivamBajpai04 <shivambajpai04@gmail.com>
2024-10-12 16:18:40 +0530

    check

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -11,20 +11,14 @@ const auth = async (req, res, next) => {
 
   try {
     console.log("try", token);
-    // const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-    // console.log(decoded);
-    req.user = {
-      user: {
-        id: "6704610d9470c14dbdcbcf07",
-        walletAddress: "0x25BCC0D02d22C458Dd613759C89486560B49ae2c",
-      },
-      iat: 1728465502,
-      exp: 1728551902,
-    };
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+    req.user = decoded
     console.log("aaa");
     next();
     console.log("aaa11");
   } catch (err) {
+    console.log(err)
     console.log("catch", token);
     res.status(401).json({ msg: "Token is not valid" });
   }
