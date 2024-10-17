4e091091375c1f10d92884bcf56354ff08ba656a
ShivamBajpai04 <shivambajpai04@gmail.com>
2024-10-17 12:09:07 +0530

    delete vote schema and update poll schema

--- backend/models/Poll.js
+++ backend/models/Poll.js
@@ -1,45 +1,50 @@
-//this will be the model for the polls. 
+//this will be the model for the polls.
 //the polls will have a status field which would either be ongoing or completed
 //a votes field that refrences the votes collection
 
-import { Schema, model } from 'mongoose';
+import mongoose, { Schema, model } from "mongoose";
 
 const pollSchema = new Schema({
   title: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   description: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   status: {
     type: String,
-    enum: ['ongoing', 'completed'],
-    default: 'ongoing'
+    enum: ["ongoing", "completed"],
+    default: "ongoing",
   },
-  votes: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Vote'
+  votes: {
+    option0: { type: Number, default: 0 },
+    option1: { type: Number, default: 0 },
+    option2: { type: Number, default: 0 },
+  },
+  voters: [{ 
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    option: { type: Number, enum: [0, 1, 2] }
   }],
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   updatedAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 // Update the 'updatedAt' field on save
-pollSchema.pre('save', function(next) {
+pollSchema.pre("save", function (next) {
   this.updatedAt = Date.now();
   next();
 });
 
-const Poll = model('Poll', pollSchema);
+const Poll = model("Poll", pollSchema);
 
 export default Poll;
