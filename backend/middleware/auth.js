ffc3641f0b9cef253cc09d004a71e3942ae51303
shivammahajan00 <shivam2314.be22@chitkara.edu.in>
2025-02-21 00:39:00 +0530

    fix: all fixes done

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,3 +1,22 @@
+
+dotenv.config();
+
+// const auth = async (req, res, next) => {
+//   const token = req.header("Authorization"); // Bearer token
+//   console.log("------------------", token);
+//   if (!token)
+//     return res.status(401).json({ msg: "No token, authorization denied" });
+
+//   try {
+//     const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded
+//     next();
+//   } catch (err) {
+//     console.log(err)
+//     res.status(401).json({ msg: "Token is not valid" });
+//   }
+// };
+
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
@@ -6,17 +25,21 @@ dotenv.config();
 const auth = async (req, res, next) => {
   const token = req.header("Authorization"); // Bearer token
   console.log("------------------", token);
-  if (!token)
+  if (!token) // Check if token is present and starts with "Bearer "
     return res.status(401).json({ msg: "No token, authorization denied" });
 
+  const actualToken = token; // Extract the actual token
+
   try {
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded
-    next();
+    const decoded = await jwt.verify(actualToken, process.env.JWT_SECRET);
+    req.user = decoded; // Attach the decoded user information to the request
+    next(); // Proceed to the next middleware or route handler
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(401).json({ msg: "Token is not valid" });
   }
 };
 
 export default auth;
+
+
